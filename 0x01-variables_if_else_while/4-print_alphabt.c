a14090c6d15e1b3989cc3a2286736db5a455a2e7
Abdelmoula Sarsari <mas2956@gmail.com>
2023-07-14 08:26:49 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Description 'print alphabets except q and e'
+ * main - Entry point
+ * Descritption 'print alphabets except q and e'
  * Return: Always 0
  */
 int main(void)

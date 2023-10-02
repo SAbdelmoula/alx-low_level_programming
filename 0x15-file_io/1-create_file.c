ec296d2a55c3518cc53ed5870b7a882ce9cce4aa
Abdelmoula Sarsari <mas2956@gmail.com>
2023-10-02 04:35:41 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - create the file
+ * @filename: pointer the name of the file to be created
+ * @text_content: pointer a string to write to the file
+ * Return: if the function fails - -1,Otherwise - 1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, z, l = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	z = write(fd, text_content, l);
+
+	if (fd == -1 || z == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
